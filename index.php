30e967d4a1ec2e09d4a0df393303014329b60987
AlanConstantino <constantinoalan98@gmail.com>
2019-07-09 20:33:08 -0700

    Revert "Please work"
    This reverts commit 8d2ba31a5c1a59605d7d5d4b28cf0221c5e0e162.

--- index.php
+++ index.php
@@ -34,43 +34,43 @@
       // $connection = new mysqli($host, $user, $password, $database);
 
       session_start();
-      // $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-      // $cleardb_server   = $cleardb_url["host"];
-      // $cleardb_username = $cleardb_url["user"];
-      // $cleardb_password = $cleardb_url["pass"];
-      // $cleardb_db       = substr($cleardb_url["path"], 1);
+      $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      $cleardb_server   = $cleardb_url["host"];
+      $cleardb_username = $cleardb_url["user"];
+      $cleardb_password = $cleardb_url["pass"];
+      $cleardb_db       = substr($cleardb_url["path"], 1);
 
-      // echo '<p>' + $cleardb_server + '<br></p>';
-      // echo '<p>' + $cleardb_username + '<br></p>';
-      // echo '<p>' + $cleardb_password + '<br></p>';
-      // echo '<p>' + $cleardb_db + '<br></p>';
+      echo '<p>' + $cleardb_server + '<br></p>';
+      echo '<p>' + $cleardb_username + '<br></p>';
+      echo '<p>' + $cleardb_password + '<br></p>';
+      echo '<p>' + $cleardb_db + '<br></p>';
 
-      // $active_group = 'default';
-      // $query_builder = TRUE;
+      $active_group = 'default';
+      $query_builder = TRUE;
 
-      // $db['default'] = array(
-      //   'dsn'    => '',
-      //   'hostname' => $cleardb_server,
-      //   'username' => $cleardb_username,
-      //   'password' => $cleardb_password,
-      //   'database' => $cleardb_db,
-      //   'dbdriver' => 'mysqli',
-      //   'dbprefix' => '',
-      //   'pconnect' => FALSE,
-      //   'db_debug' => (ENVIRONMENT !== 'production'),
-      //   'cache_on' => FALSE,
-      //   'cachedir' => '',
-      //   'char_set' => 'utf8',
-      //   'dbcollat' => 'utf8_general_ci',
-      //   'swap_pre' => '',
-      //   'encrypt' => FALSE,
-      //   'compress' => FALSE,
-      //   'stricton' => FALSE,
-      //   'failover' => array(),
-      //   'save_queries' => TRUE
-      // );
+      $db['default'] = array(
+        'dsn'    => '',
+        'hostname' => $cleardb_server,
+        'username' => $cleardb_username,
+        'password' => $cleardb_password,
+        'database' => $cleardb_db,
+        'dbdriver' => 'mysqli',
+        'dbprefix' => '',
+        'pconnect' => FALSE,
+        'db_debug' => (ENVIRONMENT !== 'production'),
+        'cache_on' => FALSE,
+        'cachedir' => '',
+        'char_set' => 'utf8',
+        'dbcollat' => 'utf8_general_ci',
+        'swap_pre' => '',
+        'encrypt' => FALSE,
+        'compress' => FALSE,
+        'stricton' => FALSE,
+        'failover' => array(),
+        'save_queries' => TRUE
+      );
 
-      // $connection = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+      $connection = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
       $full_url = "https://$_SERVER[HTTP_HOST] $_SERVER[REQUEST_URI]";
 
