1a2769412ecc68b43d6cbe7b3208ca3092c62754
AlanConstantino <constantinoalan98@gmail.com>
2019-07-09 18:38:31 -0700

    Please work :(

--- docs/pages/welcome.php
+++ docs/pages/welcome.php
@@ -31,20 +31,52 @@
     <?php
     $deployToHeroku = true;
 
-    if ($deployToHeroku) {
-      $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-      $host        = $cleardb_url["host"];
-      $user        = $cleardb_url["user"];
-      $password    = $cleardb_url["pass"];
-      $database    = substr($cleardb_url["path"], 1);
-    } else {
-      $host        = 'localhost';
-      $user        = 'root';
-      $password    = 'root';
-      $database    = 'myDB';
-    }
+    // if ($deployToHeroku) {
+    $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server   = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db       = substr($cleardb_url["path"], 1);
 
-    $connection = new mysqli($host, $user, $password, $database);
+    echo '<p>' + $cleardb_server + '<br></p>';
+    echo '<p>' + $cleardb_username + '<br></p>';
+    echo '<p>' + $cleardb_password + '<br></p>';
+    echo '<p>' + $cleardb_db + '<br></p>';
+
+    $active_group = 'default';
+    $query_builder = TRUE;
+
+    $db['default'] = array(
+      'dsn'    => '',
+      'hostname' => $cleardb_server,
+      'username' => $cleardb_username,
+      'password' => $cleardb_password,
+      'database' => $cleardb_db,
+      'dbdriver' => 'mysqli',
+      'dbprefix' => '',
+      'pconnect' => FALSE,
+      'db_debug' => (ENVIRONMENT !== 'production'),
+      'cache_on' => FALSE,
+      'cachedir' => '',
+      'char_set' => 'utf8',
+      'dbcollat' => 'utf8_general_ci',
+      'swap_pre' => '',
+      'encrypt' => FALSE,
+      'compress' => FALSE,
+      'stricton' => FALSE,
+      'failover' => array(),
+      'save_queries' => TRUE
+    );
+    // } else {
+    //   $host        = 'localhost';
+    //   $user        = 'root';
+    //   $password    = 'root';
+    //   $database    = 'myDB';
+    // }
+
+    $connection = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+    // $connection = new mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     if ($connection->connect_error)
       exit(header('Location: ../../index.php?connection=fail'));
